09269b2d3466f9b03e724ff3304be508dc003eb6
niqdev <niqdev@users.noreply.github.com>
2020-09-04 18:17:36 +0100

    add example skeleton

--- examples/src/main/scala/com/github/niqdev/Example.scala
+++ examples/src/main/scala/com/github/niqdev/Example.scala
@@ -1,6 +1,11 @@
 package com.github.niqdev
 
+import zio._
+import zio.console._
+
+// sbt -jvm-debug 5005 "examples/runMain com.github.niqdev.Example"
 object Example extends App {
 
-  println("hello")
+  override def run(args: List[String]): URIO[ZEnv, ExitCode] =
+    putStrLn("hello").exitCode
 }
